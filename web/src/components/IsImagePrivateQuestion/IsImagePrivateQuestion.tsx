16fa97f2fda7097586e67b433acbaeaee5d7a00c
RoxaneBurri <roxane.burri@etu.unige.ch>
2023-04-21 17:12:38 +0200

    feat: radio component

--- web/src/components/IsImagePrivateQuestion/IsImagePrivateQuestion.tsx
+++ web/src/components/IsImagePrivateQuestion/IsImagePrivateQuestion.tsx
@@ -1,13 +1,32 @@
+import { Box, Flex, Radio, RadioGroup, Spacer, Stack } from '@chakra-ui/react'
+
 const IsImagePrivateQuestion = () => {
+  const [value, setValue] = React.useState()
   return (
-    <div>
-      <h2>{'IsImagePrivateQuestion'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/IsImagePrivateQuestion/IsImagePrivateQuestion.tsx'
-        }
-      </p>
-    </div>
+    <Box w="220px">
+      <p>Is this image private?</p>
+      <Flex minWidth="max-content" alignItems="center">
+        <Box>
+          <p>No</p>
+        </Box>
+        <Spacer />
+        <Box>
+          <RadioGroup onChange={setValue} value={value}>
+            <Stack direction="row">
+              <Radio value="1"> </Radio>
+              <Radio value="2"> </Radio>
+              <Radio value="3"> </Radio>
+              <Radio value="4"> </Radio>
+              <Radio value="5"> </Radio>
+            </Stack>
+          </RadioGroup>
+        </Box>
+        <Spacer />
+        <Box>
+          <p>Yes</p>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
