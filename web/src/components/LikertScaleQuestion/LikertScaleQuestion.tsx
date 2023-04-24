54be72a53e59cdf70916a7e20cbe435d919d2c09
RoxaneBurri <roxane.burri@etu.unige.ch>
2023-04-24 14:03:56 +0200

    feat: generate n radio component

--- web/src/components/LikertScaleQuestion/LikertScaleQuestion.tsx
+++ web/src/components/LikertScaleQuestion/LikertScaleQuestion.tsx
@@ -9,6 +9,10 @@ type Props = {
 
 const LikertScaleQuestion = ({ n, question, leftHand, rightHand }: Props) => {
   const [value, setValue] = React.useState()
+
+  // Array of [1, 2, ..., n]
+  // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
+  const scale = Array.from({ length: n }, (_, i) => i + 1)
   return (
     <Stack minWidth="max-content" alignItems="center">
       <Text>{question}</Text>
@@ -16,11 +20,11 @@ const LikertScaleQuestion = ({ n, question, leftHand, rightHand }: Props) => {
         <Text>{leftHand}</Text>
         <RadioGroup onChange={setValue} value={value}>
           <Stack direction="row">
-            <Radio value="1" />
-            <Radio value="2" />
-            <Radio value="3" />
-            <Radio value="4" />
-            <Radio value="5" />
+            {/* https://www.pluralsight.com/guides/how-to-implement-a-component-%22loop%22-with-react 
+            create a n scale radio component*/}
+            {scale.map((item, _) => {
+              return <Radio value={item} />
+            })}
           </Stack>
         </RadioGroup>
         <Text>{rightHand}</Text>
