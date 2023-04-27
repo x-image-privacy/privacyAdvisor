c6170f6241720848ddab205479b831af09a456ac
RoxaneBurri <roxane.burri@etu.unige.ch>
2023-04-27 09:37:02 +0200

    feat: add image

--- web/src/pages/GroupAPage/GroupAPage.tsx
+++ web/src/pages/GroupAPage/GroupAPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack, StackDivider } from '@chakra-ui/react'
+import { Container, Image, Stack, StackDivider, Text } from '@chakra-ui/react'
 import { Form, Submit } from '@redwoodjs/forms'
 import LikertScaleQuestionField from 'src/components/LikertScaleQuestionField/LikertScaleQuestionField'
 import OpenEndedQuestionField from 'src/components/OpenEndedQuestionField/OpenEndedQuestionField'
@@ -6,33 +6,39 @@ import OpenEndedQuestionField from 'src/components/OpenEndedQuestionField/OpenEn
 const GroupAPage = () => {
   return (
     <Container maxW="4xl">
-      <Form onSubmit={(data) => console.log(data)}>
-        <Stack
-          direction="row"
-          spacing={4}
-          justifyContent="start"
-          divider={<StackDivider borderColor="pink.100" />}
-        >
-          <LikertScaleQuestionField
-            name="Q1"
-            n={5}
-            question="Is this image private?"
-            leftHand="No"
-            rightHand="Yes"
-          />
-          <OpenEndedQuestionField
-            question="Which elements do you consider as public in this image?"
-            name="Q2"
-            placeholder="Answer here..."
-          />
-          <OpenEndedQuestionField
-            question="Which elements feel you uncomfortable disclosing in this image?"
-            name="Q3"
-            placeholder="Answer here..."
-          />
-          <Submit>Save</Submit>
-        </Stack>
-      </Form>
+      <Stack direction="column" gap={8} alignItems="center">
+        <Text data-testid="instruction">
+          You are shown a picture and please answer some questions
+        </Text>
+        <Image src="../../../data/image1.jpg" />
+        <Form onSubmit={(data) => console.log(data)}>
+          <Stack
+            direction="row"
+            spacing={4}
+            justifyContent="start"
+            divider={<StackDivider borderColor="pink.100" />}
+          >
+            <LikertScaleQuestionField
+              name="Q1"
+              n={5}
+              question="Is this image private?"
+              leftHand="No"
+              rightHand="Yes"
+            />
+            <OpenEndedQuestionField
+              question="Which elements do you consider as public in this image?"
+              name="Q2"
+              placeholder="Answer here..."
+            />
+            <OpenEndedQuestionField
+              question="Which elements feel you uncomfortable disclosing in this image?"
+              name="Q3"
+              placeholder="Answer here..."
+            />
+            <Submit>Save</Submit>
+          </Stack>
+        </Form>
+      </Stack>
     </Container>
   )
 }
