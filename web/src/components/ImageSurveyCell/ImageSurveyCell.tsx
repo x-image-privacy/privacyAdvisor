ba6f373b79069fbe6f802ad49a7f33ddde63a764
RoxaneBurri <roxane.burri@etu.unige.ch>
2023-05-16 11:07:38 +0200

    feat: include questions cell in group A page

--- web/src/components/ImageSurveyCell/ImageSurveyCell.tsx
+++ web/src/components/ImageSurveyCell/ImageSurveyCell.tsx
@@ -1,6 +1,10 @@
 import type { ImageSurveyQuery } from 'types/graphql'
 import { CellSuccessProps, CellFailureProps, MetaTags } from '@redwoodjs/web'
 import LikertScaleQuestionField from '../LikertScaleQuestionField/LikertScaleQuestionField'
+import { Stack, StackDivider } from '@chakra-ui/react'
+import { IS_PRIVATE_QUESTION_GROUP_A, PRIVATE_ELEMENTS_QUESTION_GROUP_A, PUBLIC_ELEMENTS_QUESTION_GROUP_A } from 'web/config/constants'
+import { Submit } from '@redwoodjs/forms'
+import OpenEndedQuestionField from '../OpenEndedQuestionField/OpenEndedQuestionField'
 
 export const QUERY = gql`
   query ImageSurveyQuery($id: Int!) {
@@ -26,13 +30,34 @@ export const Success = ({
   imageSurvey,
 }: CellSuccessProps<ImageSurveyQuery>) => {
       return (
-        <ul>
-          <MetaTags title="Survey" description="Survey" />
-          {console.log("test", imageSurvey)}
-          
-          <li key={imageSurvey.id}>{JSON.stringify(imageSurvey)}</li>
-          <LikertScaleQuestionField name='test' n={5} direction='row'/>
-        </ul>
+        <Stack
+            direction="row"
+            spacing={4}
+            justifyContent="start"
+            divider={<StackDivider borderColor="grayIcon" />}
+          >
+            <LikertScaleQuestionField
+              name={IS_PRIVATE_QUESTION_GROUP_A}
+              n={5}
+              question="Is this image private?"
+              leftHand="No"
+              rightHand="Yes"
+              direction='row'
+            />
+            <OpenEndedQuestionField
+              question="Which elements do you consider as public in this image?"
+              name={PUBLIC_ELEMENTS_QUESTION_GROUP_A}
+              placeholder="Answer here..."
+            />
+            <OpenEndedQuestionField
+              question="Which elements would you feel uncomfortable disclosing in this image?"
+              name={PRIVATE_ELEMENTS_QUESTION_GROUP_A}
+              placeholder="Answer here..."
+            />
+            <Submit className="button" color="grayIcon">
+              Save
+            </Submit>
+          </Stack>
 
       )
 
--- web/src/pages/GroupAPage/GroupAPage.tsx
+++ web/src/pages/GroupAPage/GroupAPage.tsx
@@ -1,62 +1,25 @@
 import { Container, Image, Stack, StackDivider, Text } from '@chakra-ui/react'
-import { Form, Submit } from '@redwoodjs/forms'
-import LikertScaleQuestionField from 'src/components/LikertScaleQuestionField/LikertScaleQuestionField'
-import OpenEndedQuestionField from 'src/components/OpenEndedQuestionField/OpenEndedQuestionField'
-import {
-  IS_PRIVATE_QUESTION_GROUP_A,
-  PRIVATE_ELEMENTS_QUESTION_GROUP_A,
-  PUBLIC_ELEMENTS_QUESTION_GROUP_A,
-} from 'web/config/constants'
+import { Form } from '@redwoodjs/forms'
+
 
 import ImageSurveyCell from 'src/components/ImageSurveyCell'
 
 
 const GroupAPage = () => {
-  return(
-    <Form onSubmit={(data) => console.log(data)}>
+
+  return (
+    <Container maxW="6xl">
+      <Stack direction="column" gap={8} alignItems="center">
+        <Text data-testid="instruction">
+          You are shown a picture and please answer some questions
+        </Text>
+        <Image src="/data/image1.jpg" />
+        <Form onSubmit={(data) => console.log(data)}>
       <ImageSurveyCell id={2}/>
       </Form>
+      </Stack>
+    </Container>
   )
-  // return (
-  //   <Container maxW="6xl">
-  //     <Stack direction="column" gap={8} alignItems="center">
-  //       <Text data-testid="instruction">
-  //         You are shown a picture and please answer some questions
-  //       </Text>
-  //       <Image src="/data/image1.jpg" />
-  //       <Form onSubmit={(data) => console.log(data)}>
-  //         <Stack
-  //           direction="row"
-  //           spacing={4}
-  //           justifyContent="start"
-  //           divider={<StackDivider borderColor="grayIcon" />}
-  //         >
-  //           <LikertScaleQuestionField
-  //             name={IS_PRIVATE_QUESTION_GROUP_A}
-  //             n={5}
-  //             question="Is this image private?"
-  //             leftHand="No"
-  //             rightHand="Yes"
-  //             direction='row'
-  //           />
-  //           <OpenEndedQuestionField
-  //             question="Which elements do you consider as public in this image?"
-  //             name={PUBLIC_ELEMENTS_QUESTION_GROUP_A}
-  //             placeholder="Answer here..."
-  //           />
-  //           <OpenEndedQuestionField
-  //             question="Which elements would you feel uncomfortable disclosing in this image?"
-  //             name={PRIVATE_ELEMENTS_QUESTION_GROUP_A}
-  //             placeholder="Answer here..."
-  //           />
-  //           <Submit className="button" color="grayIcon">
-  //             Save
-  //           </Submit>
-  //         </Stack>
-  //       </Form>
-  //     </Stack>
-  //   </Container>
-  // )
 }
 
 export default GroupAPage
